17a866d6e345867d8f2bdd9c99a109d42661b18e
Scott Schupbach <ssschupbach@gmail.com>
2018-12-14 20:03:01 -0600

    Finish day 14 part 2

--- solutions/14.rb
+++ solutions/14.rb
@@ -11,109 +11,68 @@ class Player
     current_score + 1
   end
 
-  def adjust_index!(delta)
-    @scoreboard_index -= delta
+  def scoreboard_coords
+    @scoreboard_index.divmod(10_000).join(',')
   end
 end
 
 class RecipeGame
-  def initialize(test_recipes: Float::INFINITY, target_pattern: nil, initial_scoreboard: [3, 7])
+  def initialize(test_recipes: , target_pattern: nil, initial_scoreboard: [3, 7])
     @test_recipes = test_recipes
     @target_number_recipes = test_recipes + 10
-    @scoreboard = initial_scoreboard
+    @scoreboard = {}
+    @scoreboard_length = 0
     @pattern_index = nil
-    if target_pattern
-      if target_pattern.is_a?(Integer)
-        puts 'Warning: input is an Integer; any leading zeros will be lost!'
-        puts "\tand the resulting index will be off by the number of leading zeros"
-      end
-      @target_pattern = target_pattern.to_s
+    @target_pattern = target_pattern ? target_pattern.to_s : nil
+
+    if search?
       @target_length = @target_pattern.length
       @last_score = ''
       @last_index = -@target_length
+      check_new_recipes!(initial_scoreboard)
     end
-    check_new_recipes!(initial_scoreboard)
+
+    add_to_scoreboard!(initial_scoreboard)
     setup_players!
+  end
+
+  def search?
+    !@target_pattern.nil?
   end
 
   def setup_players!
     @players = [
-      Player.new(name: "Player 1", scoreboard_index: 0, current_score: @scoreboard[0]),
-      Player.new(name: "Player 2", scoreboard_index: 1, current_score: @scoreboard[1])
+      Player.new(name: "Player 1", scoreboard_index: 0, current_score: @scoreboard['0']),
+      Player.new(name: "Player 2", scoreboard_index: 1, current_score: @scoreboard['1'])
     ]
   end
 
   def add_to_scoreboard!(n_arr)
-    new_score_arr = n_arr.is_a?(Array) ? n_arr : [n_arr]
-    unless new_score_arr.all? { |el| el.is_a?(Integer) && el < 10 }
-      raise StandardError.new("RecipeGame#add_to_scoreboard: array malformed\n\tclass: #{n_arr[0].class}\n\tvalue: #{n_arr}")
+    n_arr.map(&:to_s).each do |char|
+      @scoreboard[@scoreboard_length.to_s] = char
+      @scoreboard_length += 1
     end
-    @scoreboard += new_score_arr
   end
 
-  def check_new_recipes(n_arr)
+  def check_new_recipes!(n_arr)
+    return unless search?
     n_arr.map(&:to_s).each do |char|
-      @last_score = (@last_score + char).split('').last(6).join('')
+      @last_score = (@last_score + char).split('').last(@target_length).join('')
       @last_index += 1
       @pattern_index = @last_score == @target_pattern ? @last_index : @pattern_index
     end
   end
 
-  def run(verbose: false)
-    @verbose = verbose
-    @round = 0
-    @progress = 0
-    @start_time = Time.now
-    @seen = 0
-
-    until target_reached? || @pattern_index # || pattern_found?
-      tick
-      @round += 1
-    end
-
-    puts "seen: #{@seen}"
-    @pattern_index || @scoreboard.slice(@test_recipes, 10).join('')
-  end
-
-  def tick
-    puts("Round: #{@round}\t\t#{@scoreboard}") if verbose?
-    @step_amount = @target_number_recipes / 100
-    curr_time = Time.now - @start_time
-    mins, secs = curr_time.divmod 60
-    hours, mins = mins.divmod 60
-    if !@step_amount.zero? && @scoreboard.length / @step_amount > @progress
-      @progress = @scoreboard.length * 100 / @target_number_recipes
-      system('clear')
-      puts "Progress:#{'*' * @progress}#{' ' * ([100 - @progress, 0].max)}|| #{@progress}%"
-      puts "\t#{"%02d" % hours}:#{"%02d" % mins}:#{"%02d" % secs}"
-    end
-    if @target_pattern && @seen > 1 && @seen % 10000 <= 1
-      system('clear')
-      puts "Current start index: #{@seen}"
-      puts "\t#{"%02d" % hours}:#{"%02d" % mins}:#{"%02d" % secs}"
-    end
-
-    target_pattern ?
-      check_new_recipes(create_new_recipes) :
-      add_to_scoreboard!(create_new_recipes)
-    move_players!
-  end
-
   def verbose?
-    @verbose
+    !!@verbose
   end
 
   def target_reached?
-    @scoreboard.length >= @target_number_recipes
+    @scoreboard_length >= @target_number_recipes
   end
 
-  def pattern_found?
-    return false if @target_pattern.nil? || @target_pattern.empty?
-    return false if @scoreboard.length - 10 < @target_pattern.length
-    @pattern_index = @scoreboard[@seen..-1].join('').index(@target_pattern)
-    @pattern_index += @seen if @pattern_index
-    @seen = @scoreboard.length - @target_pattern.length - 10
-    @pattern_index
+  def pattern_index_found?
+    !!@pattern_index
   end
 
   def create_new_recipes
@@ -124,20 +83,67 @@ class RecipeGame
 
   def move_players!
     @players.each do |player|
-      new_index = (player.scoreboard_index + player.moves) % @scoreboard.length
+      new_index = (player.scoreboard_index + player.moves) % @scoreboard_length
       player.scoreboard_index = new_index
-      player.current_score = @scoreboard[new_index]
+      player.current_score = @scoreboard[new_index.to_s].to_i
     end
+  end
+
+  def tick
+    puts("Round: #{@round}\t\tLength: #{@scoreboard_length}") if verbose?
+    @step_amount = @target_number_recipes / 100
+    curr_time = Time.now - @start_time
+    mins, secs = curr_time.divmod 60
+    hours, mins = mins.divmod 60
+    if !@step_amount.zero? && @scoreboard.length / @step_amount > @progress
+      @progress = @scoreboard.length * 100 / @target_number_recipes
+      system('clear')
+      puts "Progress:#{'*' * @progress}#{' ' * ([100 - @progress, 0].max)}|| #{@progress}%"
+      puts "\t#{"%02d" % hours}:#{"%02d" % mins}:#{"%02d" % secs}"
+    end
+
+    if search? && @scoreboard_length % 10000 <= 1
+      system('clear')
+      puts "Current start index: #{@scoreboard_length}"
+      puts "\t#{"%02d" % hours}:#{"%02d" % mins}:#{"%02d" % secs}"
+    end
+
+    new_recipes = create_new_recipes
+    check_new_recipes!(new_recipes) if search?
+    add_to_scoreboard!(new_recipes)
+    move_players!
+  end
+
+  def next_n_chars(n)
+    (0..n - 1).map { |delta| @scoreboard[(@test_recipes + delta).to_s] }.join('')
+  end
+
+  def run(verbose: false)
+    @verbose = verbose
+    @round = 0
+    @progress = 0
+    @start_time = Time.now
+    @seen = 0
+
+    until search? ? pattern_index_found? : target_reached?
+      tick
+      @round += 1
+    end
+
+    puts "seen: #{@seen}" if search?
+    puts "scoreboard length: #{@scoreboard_length}"
+    @pattern_index || next_n_chars(10)
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   # INPUT: 513401
-  verbose = ENV['VERBOSE'] == 'true'
-  target_num = (ENV['TARGET_NUM'] || 513401).to_i
-  target_pattern = ENV['TARGET_PATTERN'] || '513401'
+  truthy = [1, '1', 'true', 'TRUE', 't', 'T', 'True']
+  verbose = truthy.include? ENV['VERBOSE']
+  test_recipes = ENV['TEST_RECIPES'] || 0
+  test_recipes = test_recipes.to_i if test_recipes
+  target_pattern = ENV['TARGET_PATTERN']
   # Part 1
-  # rg = RecipeGame.new(test_recipes: target_num, initial_scoreboard: [3, 7])
-  rg = RecipeGame.new(target_pattern: target_pattern, initial_scoreboard: [3, 7])
+  rg = RecipeGame.new(test_recipes: test_recipes, target_pattern: target_pattern, initial_scoreboard: [3, 7])
   puts rg.run(verbose: verbose)
 end
